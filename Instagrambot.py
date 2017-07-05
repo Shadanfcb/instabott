309bb9443d50c59d96400d7af6e4eb511aa9891e
Shadanfcb <shadanfcb@gmail.com>
2017-07-05 12:50:21 +0530

    instabott

--- Instagrambot.py
+++ Instagrambot.py
@@ -4,14 +4,10 @@ from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
 APP_ACCESS_TOKEN = '372728299.6be5ef5.da34ffe315894053818b404943c559ef'
-#Token Owner : AVinstaBot.main
-#Sandbox Users : AVinstaBot.test0, AVinstaBot.test1, AVinstaBot.test2...... AVinstaBot.test10
 
 BASE_URL = 'https://api.instagram.com/v1/'
 
-'''
-Function declaration to get your own info
-'''
+#Function declaration to get your own info
 
 
 def self_info():
